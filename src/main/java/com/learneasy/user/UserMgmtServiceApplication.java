e34d133e5b28a400c2e4f21d03bd89d2f7edb621
Sandeep Chandan <enquiresandeep@gmail.com>
2023-02-21 22:57:08 -0600

    added comment

--- src/main/java/com/learneasy/user/UserMgmtServiceApplication.java
+++ src/main/java/com/learneasy/user/UserMgmtServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserMgmtServiceApplication {
-
+//some commntgi
 	public static void main(String[] args) {
 		SpringApplication.run(UserMgmtServiceApplication.class, args);
 	}
